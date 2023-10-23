6732562d32d6e4f1c87edfdb5df5cb12eea696ec
MaxDanna26 <maximilianodanna@hotmail.com>
2023-10-23 17:36:03 +0200

    FuncionesPublish

--- DannaAereaConsultas.sql
+++ DannaAereaConsultas.sql
@@ -1,5 +1,4 @@
 -- Lista de vuelos con nombre de piloto, ordenados por fecha
-
 SELECT Nombre as NombrePiloto,Apellido as ApellidoPiloto,CodigoVuelo,FK_NombreAvion as NombreAvion,Fecha,DuracionMin,
 FK_Origen as Origen,FK_Destino as Destino,FK_IdPiloto as Piloto ,FK_IdCopiloto as Copiloto FROM DatosPersonales DT
 	INNER JOIN Piloto P
@@ -9,7 +8,7 @@ ON P.IdPiloto = V.FK_IdPiloto
 ORDER BY V.Fecha
 
 -- Num. de pasajero en cada vuelo (o en un vuelo concreto)
-SELECT * FROM Pasajero
+SELECT Count(*) FROM Pasajero
 WHERE FK_CodigoVuelo = 5
 
 -- Lista de pilotos que hacen Intercontinental
@@ -50,10 +49,24 @@ WHERE V.FK_Destino != (SELECT IdCiudad
 FROM Ciudad
 WHERE NombreCiudad = 'Barcelona')
 
+-- Solucionado
+SELECT Nombre,Apellido FROM DatosPersonales DT
+WHERE IdPersonal IN(1,2,3,4)
+EXCEPT 
+SELECT Nombre,Apellido FROM DatosPersonales DT
+	INNER JOIN Piloto P
+ON DT.IdPersonal = P.FK_IdPersonal
+	INNER JOIN Vuelo V
+ON V.FK_IdPiloto = P.IdPiloto
+WHERE V.FK_Destino = (SELECT IdCiudad 
+FROM Ciudad
+WHERE NombreCiudad = 'Barcelona')
+
 -- Num. de vuelos pilotados por pilotos intercontinentales
 SELECT COUNT(*) AS ConteoVuelosPorPiloto
 FROM Vuelo
-WHERE Vuelo.FK_IdPiloto IN (
+WHERE Vuelo.FK_IdPiloto 
+IN (
     SELECT IdPiloto
     FROM Piloto
     WHERE HaceIntercontinental = 1)
---
+++ FuncionesSQL.sql
@@ -0,0 +1,47 @@
+
+
+ALTER FUNCTION FechaDeHoy
+( 
+)
+RETURNS date
+AS
+BEGIN 
+	DECLARE @resultado date;
+	SET @resultado = getdate();
+	RETURN @resultado;
+END
+
+SELECT dbo.FechaDeHoy()
+
+-- FUNCION QUE DEVUELVA EL NUMERO DEL DIA
+
+CREATE FUNCTION DayToday
+()
+RETURNS int
+AS
+BEGIN 
+	DECLARE @resultado int;
+	SET @resultado = DAY(getdate());
+	RETURN @resultado;
+END
+
+
+SELECT dbo.DayToday()
+
+ALTER FUNCTION ValorDeHoy(@opcion int)
+RETURNS INT
+AS
+BEGIN 
+DECLARE @resultado INT;
+
+SET @resultado = 
+CASE @opcion
+	WHEN 1 THEN DAY(getdate())
+	WHEN 2 THEN MONTH(getdate())
+	WHEN 3 THEN YEAR(getdate())
+END
+
+	RETURN @resultado
+END
+
+SELECT dbo.ValorDeHoy(2)
